ef30565bac735336687a98a969118dad8d76cbe2
natmark <natmark0918@gmail.com>
2018-03-29 23:50:24 +0900

    :new: Add project files

---
+++ HakoBus-v2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HakoBus-v2
+//
+//  Created by AtsuyaSato on 2018/03/29.
+//  Copyright © 2018年 Atsuya Sato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
